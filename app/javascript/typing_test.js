deb18a5d1d265e2c747f584d3354c1e9b1ed03bf
Dylan <connorsdylan18@gmail.com>
2023-02-13 14:45:58 +0000

    Typing test has core features DONE

--- app/javascript/typing_test.js
+++ app/javascript/typing_test.js
@@ -1,68 +1,87 @@
-let inputField = document.getElementById("input");
-let words = document.getElementById("text");
-let startFlag = false;
+function startTypingTest() {
+  return new Promise((resolve, reject) => {
+    let inputField = document.getElementById("input");
+    let wordsValue = document.getElementById("text").textContent;
+    let characters = wordsValue.split("");
+    let inputValue;
+    let startFlag = false;
+    let start;
+    let errors = 0;
+    let uErrors = 0;
+    let entries = 0;
 
-function startTypingTest(startFlag) {
+    inputField.addEventListener("input", function() {
+      inputValue = inputField.value;
+      let prevLength = inputValue.length - 1;
+      
+      if (inputValue.length === prevLength + 1) {
 
-  let start;
-  let errors = 0;
-  let uErrors = 0;
-  let entries = 0;
+        entries += 1;
 
-  inputField.addEventListener("input", function() {
+        if (!startFlag) {
+          start = new Date();
+          startFlag = true;
+        } 
 
-    let inputValue = inputField.value;
-    let prevLength = inputValue.length - 1;
-    let wordsValue = words.textContent;
-    
-    if (inputValue.length === prevLength + 1) {
-
-      entries += 1;
-
-      if (!startFlag) {
-        start = new Date();
-        startFlag = true;
-      } 
-
-      if (inputValue[inputValue.length - 1] != wordsValue[inputValue.length - 1]) {
-        document.getElementById("input").classList.add("incorrect");
-        errors += 1;
-      } else {
-        document.getElementById("input").classList.remove("incorrect");
-      }
-
-      if (inputValue.length === wordsValue.length) {
-        let end = new Date();
-        let time = (end - start)/60000;
-
-        document.getElementById("typing-test").classList.add("hide");
-
-        for (let i = 0; i < wordsValue.length; i++) {
-          if (inputValue[i] !== wordsValue[i]) {
-            uErrors += 1;
-          }
+        if (inputValue[inputValue.length -1] !== characters[inputValue.length - 1]) {
+          console.log(inputValue[inputValue.length -1], characters[inputValue.length - 1])
+          errors += 1;
         }
 
-        return time, uErrors, errors, entries
-      
-      }
-    }
+        if (inputValue.length === wordsValue.length) {
+          let end = new Date();
+          let time = (end - start)/60000;
+
+          document.getElementById("typing-test").classList.add("hide");
+          document.getElementById("results").classList.remove("hide");
+
+          for (let i = 0; i < wordsValue.length; i++) {
+            if (inputValue[i] !== wordsValue[i]) {
+              uErrors += 1;
+            }
+          }
+
+          console.log(time, uErrors, errors, entries)
+          resolve({ time, uErrors, errors, entries });
+        
+        }
+      } 
+    });
   });
 }
 
 
 function calculateResults(time, uErrors, errors, entries) {
+  console.log(time)
+  let accuracy
   let gWPM = (entries/5)/time;
   let nWPM = gWPM - (uErrors/time);
-  let accuracy = errors/entries * 100
-  return gWPM, nWPM, accuracy
+  if (errors === 0) {
+    accuracy = 100;
+  } else {
+    accuracy = 100 - (errors/entries * 100);
+  }
+  return {grossWPM: gWPM, netWPM: nWPM, totalAccuracy: accuracy, time: time};
 }
 
-function displayResults() {
-  //
+
+function displayResults(results) {
+  console.log(results.time)
+  const resultsArray = [ {id: "wpm", value: results.netWPM}, {id: "accuracy", value: results.totalAccuracy}, {id: "gross", value: results.grossWPM}, {id: "time", value: results.time} ];
+  resultsArray.forEach(item => { 
+    document.getElementById(item.id).textContent = item.value.toFixed(2);
+  });
 }
 
 
-let timeElapsed, uncorrectedErrors, totalErrors, allTypedEntries = startTypingTest(startFlag);
-let grossWPM, netWPM, totalAccuracy = calculateResults(timeElapsed, uncorrectedErrors, totalErrors, allTypedEntries);
-
+startTypingTest()
+  .then(({ time, uErrors, errors, entries }) => {
+    console.log(time)
+    return calculateResults(time, uErrors, errors, entries);
+  })
+  .then((results) => {
+    displayResults(results);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email adress"}
+  validates :password, presence: true
+  validates :password, format: { with: /[A-Z]/, message: "must include one uppercase letter"}
+  validates :password, format: { with: /\d/, message: "must include one number"}
+  validates :password, length: { minimum: 8, message: "must be at least 8 characters long"}
 end
