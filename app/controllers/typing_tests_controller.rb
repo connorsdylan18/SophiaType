6430451fe49fe684aaeab3b4ae5bb4c820967a13
Dylan <connorsdylan18@gmail.com>
2023-02-17 15:12:22 +0000

    Controller handles different modes

--- app/controllers/typing_tests_controller.rb
+++ app/controllers/typing_tests_controller.rb
@@ -5,7 +5,20 @@ class TypingTestsController < ApplicationController
     if @mode == "quote"
       @extract = Extract.where(extract_length: @size).order("RANDOM()").first
       @text = @extract.extract_text.strip 
+    elsif @mode == "words" || @mode == "timed"
+      @extract = Extract.find_by(extract_title: "WORDS")
+      if @mode == "words"
+        case @size 
+        when "small"
+          @text = @extract.extract_text.split.sample(25).join(" ")
+        when "medium"
+          @text = @extract.extract_text.split.sample(50).join(" ")
+        when "large"
+          @text = @extract.extract_text.split.sample(75).join(" ")
+        end
+      else  
+        @text = @extract.extract_text.split.sample(25).join(" ")
+      end
     end
-    
   end
-end
+end 
--- app/javascript/main_index_script.js
+++ app/javascript/main_index_script.js
@@ -3,9 +3,9 @@ let selectedOption;
 
 btnGroup.addEventListener("click", (event) => {
   selectedOption = document.querySelector('input[name="mode"]:checked').dataset.value;    
-  if (selectedOption === "quote") {
-    document.querySelector("#size-select").classList.remove("hide");
-  } else {
+  if (selectedOption === "timed") {
     document.querySelector("#size-select").classList.add("hide");
+  } else {
+    document.querySelector("#size-select").classList.remove("hide");
   }
 });
